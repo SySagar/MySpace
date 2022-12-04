3742eff1156a4147f965cba69343d6771ae7f95c
SySagar <sysagar07@gmail.com>
2022-12-04 16:41:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const deg =6;
+  const hr = document.querySelector('#hr');
+  const mn = document.querySelector('#mn');
+  const sc = document.querySelector('#sc');
+
+  setInterval(()=>{
+    let day = new Date();
+    let hh = day.getHours()*30;
+    let mm = day.getMinutes()*deg;
+    let ss = day.getSeconds()*deg;
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    
+    sc.style.transform = `rotateZ(${ss}deg)`;
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='main flex flex-row border border-black min-h-screen max-h-screen overflow-hidden bg-backcolor justify-center items-center'>
+
+        <div className="clock w-[350px] h-[350px] flex bg-clockcolor rounded-full shadow-2xl justify-center items-center">
+
+          <div className="hour absolute w-[160px] h-[160px]">
+            <div className="hr absolute w-[160px] h-[160px] flex justify-center items-start" id='hr'></div>
+          </div>
+
+          <div className="min absolute w-[190px] h-[190px]">
+            <div className="mn absolute w-[190px] h-[190px] flex justify-center items-start" id='mn'></div>
+          </div>
+
+          <div className="sec absolute  w-[230px] h-[230px]">
+            <div className="sc absolute w-[230px] h-[230px] flex justify-center items-start" id='sc'></div>
+          </div>
+
+          <div className="indicators relative flex justify-center items-center">
+
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+
+          </div>
+
+        </div>
+      </div>
+
+
     </div>
   );
 }
