b9319768b245143f0d35137c931cd8dca3fbce87
SySagar <sysagar07@gmail.com>
2022-12-05 00:57:19 +0530

    added weather elements

--- src/App.js
+++ src/App.js
@@ -1,81 +1,33 @@
 import './App.css';
 import React from 'react';
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+import SearchBar from './SearchBar'
+import Clock from './Clock'
+import Activity from './Activity'
+import Stack from '@mui/material/Stack';
 
-    // Initializing the state 
-    this.state = { color: 'lightgreen' };
-  }
-  componentDidMount() {
+export default function App() {
 
-    const deg = 6;
-    const hr = document.querySelector('#hr');
-    const mn = document.querySelector('#mn');
-    const sc = document.querySelector('#sc');
+  return (
+    <div className="App">
+      <div className='main flex flex-row border border-black min-h-screen max-h-screen overflow-hidden bg-backcolor justify-evenly items-center gap-64'>
 
-    setInterval(() => {
-      let day = new Date();
-      let hh = day.getHours() * 30;
-      let mm = day.getMinutes() * deg;
-      let ss = day.getSeconds() * deg;
-
-      hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
-
-      mn.style.transform = `rotateZ(${mm}deg)`;
-
-      sc.style.transform = `rotateZ(${ss}deg)`;
-    })
-
-  }
-  render() {
-
-    return (
-      <div className="App">
-
-        <div className='main flex flex-row border border-black min-h-screen max-h-screen overflow-hidden bg-backcolor justify-center items-center'>
-
-          <div className="clock w-[350px] h-[350px] flex bg-clockcolor rounded-full shadow-2xl justify-center items-center">
-
-            <div className="hour absolute w-[160px] h-[160px]">
-              <div className="hr absolute w-[160px] h-[160px] flex justify-center items-start" id='hr'></div>
-            </div>
-
-            <div className="min absolute w-[190px] h-[190px]">
-              <div className="mn absolute w-[190px] h-[190px] flex justify-center items-start" id='mn'></div>
-            </div>
-
-            <div className="sec absolute  w-[230px] h-[230px]">
-              <div className="sc absolute w-[230px] h-[230px] flex justify-center items-start" id='sc'></div>
-            </div>
-
-            <div className="indicators relative flex justify-center items-center">
-
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-
-            </div>
-
-          </div>
+        <div className="Clock">
+          <Clock></Clock>
         </div>
 
-        <script src='MyScript.js'></script>
 
+<Stack gap={5}>
 
+        <div className="search">
+          <SearchBar></SearchBar>
+        </div>
+
+        <div className="quotes">
+          <Activity></Activity>
+        </div>
+
+</Stack>
       </div>
-    );
-
-
-  }
+      </div>
+      );
 }
-export default App;
